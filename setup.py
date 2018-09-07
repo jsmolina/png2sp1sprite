a6219dff78377aa9743312b5a03a138c531cc931
Jordi Sesmero <jsmolina@gmail.com>
2018-09-07 12:19:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
