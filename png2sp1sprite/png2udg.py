47c7ec129e6a7e3866d21e363be25c303af4e61f
Jordi Sesmero <jsmolina@gmail.com>
2018-10-11 23:21:10 +0200

    change unsigned char to uint8_t

--- png2sp1sprite/png2udg.py
+++ png2sp1sprite/png2udg.py
@@ -102,7 +102,7 @@ def main():
 
         rows.append(hex_formatted(col))
     fmt = "{}".format(', '.join(rows))
-    print("unsigned char " + name + "[] = {" + fmt + "};")
+    print("const uint8_t " + name + "[] = {" + fmt + "};")
 
 if __name__ == "__main__":
     main()
