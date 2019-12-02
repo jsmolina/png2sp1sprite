87fd605790fc81a6a3560f375d059cc78920893e
jordism <jordism@tid.es>
2019-12-02 15:07:57 +0100

    simplify conversion

--- png2sp1sprite/png2btitle.py
+++ png2sp1sprite/png2btitle.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
-__version__ = "1.0.1"
+__version__ = "0.0.1"
+import math
 from array import array
 from argparse import ArgumentParser
 from PIL import Image
-
+RGB_COLOR_COUNT = 256
+GRAYSCALE_COLOR_COUNT = 256
 
 def get_int_from_rgb(rgb):
-    red = rgb[0]
-    green = rgb[1]
-    blue = rgb[2]
-    print red, green, blue
-    rgb_int = (red<<16) + (green<<8) + blue
-    return rgb_int
+    if isinstance(rgb, tuple):
+        red = rgb[0]
+        green = rgb[1]
+        blue = rgb[2]
+        rgb_int = (red<<16) + (green<<8) + blue
+        return rgb_int
+    else:
+        return rgb
 
 
 def get_bitmap_value(rgb):
@@ -23,7 +27,26 @@ def get_bitmap_value(rgb):
 
 
 def get_attribute_value(rgb):
-    return "{0:b}".format(get_int_from_rgb(rgb))
+    value = get_int_from_rgb(rgb)
+    if value > 15:
+        print("WARNING! got color value of {0}, downgrading to 15".format(value))
+        value = 15
+    attr = "{0:b}".format(value)
+    return attr
+
+
+def changeColorDepth(image, colorCount):
+    if image.mode == 'L':
+        raito = GRAYSCALE_COLOR_COUNT / colorCount
+        change = lambda value: math.trunc(value / raito) * raito
+        return image.point(change)
+
+    if image.mode == 'RGB' or image.mode == 'RGBA':
+        raito = RGB_COLOR_COUNT / colorCount
+        change = lambda value: math.trunc(value / raito) * raito
+        return Image.eval(image, change)
+
+    raise ValueError('{mode} error'.format(mode=image.mode))
 
 
 def main():
@@ -34,6 +57,7 @@ def main():
                             )
 
     parser.add_argument("--version", action="version", version="%(prog)s "  + __version__)
+    parser.add_argument("--convert", action="store_true", default=False)
 
     parser.add_argument("image", help="image to convert", nargs="?")
 
@@ -50,13 +74,15 @@ def main():
         exit(1)
         return
 
+    convert = args.convert
     mask = None
 
     # force 8 bit per pixel
-    #image = image.convert('P')
+    if convert:
+        image = image = image.convert('P')
     # Byte 1: bitmap value for 1st pixel line, 1st column (0-8)
     bloques = array('B')
-    
+
     # todo what about larger images?
     for y in range(16):
         for x in range(0, 16, 8):
@@ -69,8 +95,9 @@ def main():
         for y in range(0, 16, 2):
             # Byte 33: attribute value for 1st and 2nd pixel line, 1st column
             column_bits = ""
-            column_bits += get_attribute_value(image.getpixel((x, y)))
-            column_bits += get_attribute_value(image.getpixel((x, y + 1)))
+            pixelline_one = get_attribute_value(image.getpixel((x, y)))
+            pixelline_two = get_attribute_value(image.getpixel((x, y + 1)))
+            column_bits = pixelline_one + pixelline_two
 
             bloques.append(int(column_bits, 2))
 
